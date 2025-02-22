cd624e63f4b960f70ff172ee13a4789812adbcbe
Otto0222 <otto0222@gmail.com>
2025-02-21 20:11:40 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,20 +1,13 @@
 df = pd.read_csv("https://github.com/Otto0222/ejercicio/blob/main/Credit.csv")
 df.drop(columns=["Unnamed: 0"], inplace=True)
 # Ver información general del dataset
-df.info()
-plt.figure(figsize=(8, 5))
-fig1 = sns.histplot(df["Balance"], bins=30, kde=True, color='blue')
-plt.title("Distribución de la Variable Balance")
-plt.xlabel("Balance")
-plt.ylabel("Frecuencia")
-plt.show()
+fig1 = px.histogram(df, x="Balance", title="Distribución de la Variable Balance")
+fig1.show()
 
 #Display plot 1
 st.plotly_chart(fig1, use_container_width=True)
 
-plt.figure(figsize=(10, 6))
-fig2 = sns.heatmap(df.corr(numeric_only=True), annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
-plt.title("Matriz de Correlación entre Variables Numéricas")
-plt.show()
- 
+fig2 = px.imshow(df.corr(numeric_only=True), title="Matriz de Correlación entre Variables Numéricas")
+
+fig2.show()
 st.plotly_chart(fig2, use_container_width=True)
